0e658b838f314791cb1ad304d2ba1ea4859c1230
Kamuhh <contacto@publicarlos.cl>
2025-08-08 19:47:50 -0400

    Update kivicare-clinic-management-system.php

--- wp-content/plugins/kivicare-clinic-management-system/kivicare-clinic-management-system.php
+++ wp-content/plugins/kivicare-clinic-management-system/kivicare-clinic-management-system.php
@@ -65,8 +65,20 @@ register_deactivation_hook( __FILE__, [KCDeactivate::class, 'deActivate'] );
 ( new KCDeactivate() );
 
 do_action('kivicare_activate_init');
+do_action('kivicare_activate_init');
 
 add_action('admin_enqueue_scripts', function () {
     wp_enqueue_script('thickbox');
     wp_enqueue_style('thickbox');
-});
+
+    wp_dequeue_script('kc_custom');
+    wp_deregister_script('kc_custom');
+
+    wp_enqueue_script(
+        'kivicare-custom',
+        KIVI_CARE_DIR_URI . 'assets/js/custom.js',
+        array('jquery', 'thickbox'),
+        '3.6.11.2',
+        true
+    );
+}, 20);
